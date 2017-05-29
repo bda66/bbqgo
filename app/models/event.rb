da4905d02a4c17dbcd5d3a615d97fd83a8c89a99
Dmitry Belyaev <didima220@mail.ru>
2017-05-29 19:38:26 +0300

    Bootstrap installed

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,9 @@
 class Event < ApplicationRecord
+
+  belongs_to :user
+
+  validates :user, presence: true
+
   validates :title, presence: true, length: {maximum: 255}
   validates :adress, presence: true
   validates :datetime, presence: true
